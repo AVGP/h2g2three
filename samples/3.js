6c376d98764e00ac8cbc29feab0c6162c023ebba
Martin N. <mr.avgp@googlemail.com>
2015-03-16 11:21:28 +0100

    Update 3.js

--- samples/3.js
+++ samples/3.js
@@ -1,8 +1,8 @@
 var loader = new OBJMTLLoader();
 var ship = undefined;
 
-loader.load("models/Feisar_Ship.obj",
-"models/Feisar_Ship.mtl", function(obj3d) {
+loader.load("models/FeisarShip.obj",
+"models/FeisarShip.mtl", function(obj3d) {
   window.thing = obj3d;
   obj3d.scale.set(0.5, 0.5, 0.5);
   scene.add(obj3d);
